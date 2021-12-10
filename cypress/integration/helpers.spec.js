5735e7a4641cea070d441265d9654a61f2871eac
Lana Schuster <lanaschuster1@gmail.com>
2021-12-10 14:41:21 -0300

    test: its

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -33,4 +33,36 @@ describe('Helpers', () => {
       return 2 
     }).should('be.equal', 2)
   })
+
+  it('Its - exemplo 1', () => {
+    const obj = { nome: 'user', idade: 26 }
+    
+    cy.wrap(obj).should('have.property', 'nome')
+    cy.wrap(obj).its('nome').should('be.equal', 'user')
+  })
+
+  it('Its - exemplo 2', () => {
+    const obj = { 
+      nome: 'user', 
+      idade: 26,
+      endereco: {
+        rua: 'Rua xpto'
+      }
+    }
+    
+    cy.wrap(obj).should('have.property', 'endereco')
+    
+    cy.wrap(obj)
+      .its('endereco')
+      .should('have.property', 'rua')
+    
+    cy.wrap(obj)
+      .its('endereco')
+      .its('rua')
+      .should('be.equal', 'Rua xpto')
+    
+    cy.wrap(obj)
+      .its('endereco.rua')
+      .should('be.equal', 'Rua xpto')
+  })
 })
